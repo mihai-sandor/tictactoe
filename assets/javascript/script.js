4f7d6b0bd64d509fe49d3f62375b0193f918376d
Sandor Mihai <mihai93dh@gmail.com>
2026-07-15 05:44:22 +0300

    click on the box mark X or O

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,3 +7,21 @@ let curentPlayer = "X";
 let gameActive  = true;
 // We take all the HTML boxes to add functionality to them
 const cells = document.querrySelectorAll(".cell");
+
+function handleCellClick(event) {
+    const cell = event.target;
+    const index = cell.dataset.index // we read data-index fron HTML
+
+    // We don't do anything if the game is over or the house is busy
+    if (!gameActive || board[index] !== null) {
+        
+        // We save the move to the status array
+        board[index] = curentPlayer;
+
+        // We display the symbol in the box and add the class for color
+        cell.textContent = curentPlayer;
+        cell.classList.add(curentPlayer.toLowerCase());
+    }
+}
+// We attach the above function to each box
+cell.forEach(cell => cell.addEventListener("click", handleCellClick));
